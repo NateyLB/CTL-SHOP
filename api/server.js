3ed4fd9414f13b634598aa7b6c7083270c2dceaf
Nathan <nateyl.b@gmail.com>
2020-06-13 04:26:39 -0700

    configured cors

--- api/server.js
+++ api/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const adminRouter = require("./admin/admin-router.js");
-
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 server.use("/api/admin", adminRouter);
 
