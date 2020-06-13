f64baba3884807eddb7231c9ae9c8736fc8b88d3
Nathan <nateyl.b@gmail.com>
2020-06-13 03:51:09 -0700

    hide keys

--- api/admin/config-vars.js
+++ api/admin/config-vars.js
@@ -1,6 +1,4 @@
 module.exports = {
-    jwtSecret: process.env.JWT_SECRET || "keepitsecretKEEPITTIGHT",
-    aws_access_key_id: 'AKIAJGQYOEONZX7OA6DA',
-    aws_secret_access_key: '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'
+    jwtSecret: process.env.JWT_SECRET || "keepitsecretKEEPITTIGHT"
 }
 
